66c5f9455058c0f4cd7af41b084b36dcc03264d9
ElenaDas <153499927+ElenaDas@users.noreply.github.com>
2023-12-17 00:33:49 +0200

    hash_script.sh

--- hash_script.sh
+++ hash_script.sh
@@ -14,11 +14,11 @@ if [[ $user_input =~ ^[0-9]{4}$ ]]; then
     # Convert the hexadecimal hash to decimal
     dec_hash=$(printf "%d\n" "0x$hex_hash")
 
-    echo "Generated 4-digit decimal hash: $dec_hash"
-    echo "Generated 4-digit hexadecimal hash: $hex_hash"
+    echo "4-digit decimal hash: $dec_hash"
+    echo "4-digit hexadecimal hash: $hex_hash"
 
     # Save the output to output.txt
-    echo -e "Generated 4-digit decimal hash: $dec_hash\nGenerated 4-digit hexadecimal hash: $hex_hash" > hash_output.txt
+    echo -e "4-digit decimal hash: $dec_hash\n4-digit hexadecimal hash: $hex_hash" > hash_output.txt
     echo "Output saved to hash_output.txt"
 else
     echo "Invalid input. Please enter a 4-digit integer."
