be064f165715fec5a11308ec1afd328a3f2cfd2e
ElenaDas <153499927+ElenaDas@users.noreply.github.com>
2023-12-17 00:29:26 +0200

    hash_script.sh

--- hash_script.sh
+++ hash_script.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-# Get the 4-digit integer from the command line
-input=$1
+# Get user input for the 4-digit integer
+read -p "Enter a 4-digit integer: " user_input
 
-# Check if input is provided
-if [ -z "$input" ]; then
-    echo "Usage: $0 <4-digit-integer>"
-    exit 1
+# Check if the input is a 4-digit integer
+if [[ $user_input =~ ^[0-9]{4}$ ]]; then
+    # Hash the input using SHA-256
+    hash_output=$(echo -n "$user_input" | sha256sum)
+
+    # Extract the first 4 characters of the hash
+    hex_hash=$(echo "$hash_output" | cut -c1-4)
+
+    # Convert the hexadecimal hash to decimal
+    dec_hash=$(printf "%d\n" "0x$hex_hash")
+
+    echo "Generated 4-digit decimal hash: $dec_hash"
+    echo "Generated 4-digit hexadecimal hash: $hex_hash"
+
+    # Save the output to output.txt
+    echo -e "Generated 4-digit decimal hash: $dec_hash\nGenerated 4-digit hexadecimal hash: $hex_hash" > hash_output.txt
+    echo "Output saved to hash_output.txt"
+else
+    echo "Invalid input. Please enter a 4-digit integer."
 fi
-
-# Use SHA-256 hashing algorithm to hash the input
-hash_output=$(echo -n "$input" | sha256sum)
-
-# Save the hash output to hash_output.txt
-echo "$hash_output" > hash_output.txt
-
